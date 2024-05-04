6ef4a1b71f036fffc815759aaece1455392b04dc
Dushyant418 <dushyantlochab335@gmail.com>
2024-05-04 11:43:18 +0530

    js added

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
+const navlink = document.querySelectorAll(".nav-links")
+navlink.forEach(a => a.addEventListener("click", () => {
+    menulist.classList.remove("show");
+    document.body.classList.remove("overflow-hidden");
+    menuicon.classList.toggle("show");
+}))
+menuicon.addEventListener("click", () => {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overflow-hidden");
+    menuicon.classList.toggle("show");
+});
